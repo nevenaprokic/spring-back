926f9a478fd594332313e3a803eb0a62530a14c4
Nevena Prokic <prokic.sw6.2019@uns.ac.rs>
2022-06-14 16:23:50 +0200

    Konfiguracija.

--- src/main/java/com/booking/ISAbackend/config/WebConfig.java
+++ src/main/java/com/booking/ISAbackend/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     // Za svrhe razvoja konfigurisemo dozvolu za CORS kako ne bismo morali @CrossOrigin anotaciju da koristimo nad svakim kontrolerom
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowedOrigins("https://isa-react-front.herokuapp.com");
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowedOrigins("https://react-front-isa-mrs.herokuapp.com");
     }
 
     @Bean
